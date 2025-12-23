eccac86a8cb2f464da050ac9c2918cfc1d6a92cb
Anas Abazid <anasabazid483@gmail.com>
2025-12-23 12:25:32 +0100

    Array Example

---
+++ ArrayExample.py
@@ -0,0 +1,14 @@
+#Creating an array
+array = []
+
+#inserting values in the array
+array.append(1)
+array.append(3)
+array.append(5)
+# Print all elements in the list
+print("Elements of an array: "+ str(array))
+
+#Changing the value of array[0]
+array[0] = 2
+print("Elements of an array after changes: " + str(array))
+print("First element of an array: " + str(array[0]))
